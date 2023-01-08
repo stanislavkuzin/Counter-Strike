d494d0b6bfe660e7e784c3e2ec5b8de6b31c724f
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-01-08 16:58:39 +0100

    v_0.17 localstorage

--- scene.js
+++ scene.js
@@ -10,7 +10,10 @@ let menuWeapon;
 let startTime;
 let countMap = 0;
 let countBot = -1;
-
+let data = {
+    'data-count_shoots': 0,
+    'data-count_hits': 0
+}
 class Scene {
     constructor(value, mapLevel) {
         this.value = value;
@@ -41,14 +44,12 @@ const openNewGame = () => {
     let observer = new Observer(); // M
     botOnLevel = new CreateBot(botName[countBot]);
     observer.subscribe(botOnLevel.damageBot);
-    console.log(observer);
     setTimeout(observer.unsubscribe(botOnLevel.damageBot), 3000);
-    setTimeout(console.log(observer), 3000);
-    ;
-    document.querySelector('.bot-duck').addEventListener('click', (event) => { observer.broadcast(event)}); // add Listener for observer
+    document.querySelector('.bot-duck').addEventListener('click', (event) => { observer.broadcast(event) }); // add Listener for observer
     playerGame = new Player('./img/ak471.png');
     menuWeapon = new MenuWeapon();
-    new sceneTextMenu;
+    localStorage.setItem('data', JSON.stringify(data));
+    let sceneMenu = new sceneTextMenu;
     audio.audioMenu.pause();
     audio.audioGame.volume = '0.1';
     audio.audioGame.play();
@@ -57,6 +58,7 @@ const openNewGame = () => {
     body.addEventListener('keydown', closeMap);
     body.removeEventListener('keydown', newsGame);
     scene.addEventListener('click', hit);
+    scene.addEventListener('click', () => {drawResults(localStorage)});
     startTime = setTimeout(() => playerGame.huckPlayer(), 1000);
 }
 
@@ -75,6 +77,16 @@ const closeMap = (event) => {
 }
 
 newGame.addEventListener('click', openNewGame); // new game button (LoadScrean)
+
+function drawResults(localStorage) {
+    let temp = JSON.parse(localStorage.getItem('data'));
+    let count = 0;
+    let divArr = Array.from(document.querySelectorAll('.box_in'));
+    for (const item in temp) {
+           divArr[count].textContent = temp[item]; 
+        count += 1;
+    }
+}
 
 
 
